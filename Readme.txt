063e2bf2aa2769aa08ef4e49f2246695409bea05
Maria <maria.carranzavaldes@gmail.com>
2022-05-01 15:29:08 +0200

    Repaso

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Desde la semana que viene se regulara la entrada a Barcelona en auto dependiendo del numero 
+de patente.
+Las patentes inferiores a 1000 puede conducir solo martes y jueves,
+entre 1000 y 1799, los dias lunes y miercoles y apatir de 1800 incluido, libre conduccion.
+
---
+++ index.js
@@ -0,0 +1,13 @@
+let patente = 1400
+
+if (patente < 1000) {
+    console.log("Puedes conducir dentro de Barcelona martes y jueves")
+ } else if 
+        (patente > 1000 && patente <1800 ) {
+            console.log ("Puedes conducir dentro de barcelona los dias lunes y miercoles");
+        }
+else if (patente >= 1800) {
+    console.log("No tienes restricciones")
+};
+
+ 
